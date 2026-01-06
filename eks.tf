9dfd59d9fe7b27b373e8795ed2f64e3a1b8198fd
Danilo Augusto <danilloagt@gmail.com>
2026-01-05 21:34:13 -0300

    feat: update roles e vpc

--- eks.tf
+++ eks.tf
@@ -5,8 +5,8 @@ resource "aws_eks_cluster" "this" {
 
   vpc_config {
     subnet_ids = [
-      "subnet-06cc1d21fa59e4630",
-      "subnet-03761aa7a16b7b2bf"
+      "subnet-068e259ae144edb0e",
+      "subnet-0d7b6abad5df63724"
     ]
   }
 }
